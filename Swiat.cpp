11f19b7bca2dc691698dae6c8b477e4c99041c6a
Mateusz Szymanowski <mathew.szyman@gmail.com>
2017-04-20 23:20:08 +0200

    ADD: srand function in Swiat constructor; CHANGE: map orientation

--- Swiat.cpp
+++ Swiat.cpp
@@ -1,11 +1,15 @@
 #include"Swiat.h"
 #include"Organizm.h"
+#include"Czlowiek.h"
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
 Swiat::Swiat(int width, int height)
 	:width(width), height(height)
 {
+	srand(time(NULL));
 	map = new Organizm**[height];
 	for (int i = 0; i < height; i++)
 	{
@@ -15,7 +19,7 @@ Swiat::Swiat(int width, int height)
 	for(int i=0; i<height; i++)
 		for (int j = 0; j < width; j++)
 		{
-			map[i][j] = new Zwierze(0, 0, this, i, j);
+			map[j][i] = new Zwierze(0, 0, this, i, j);
 		}
 	
 }
@@ -29,23 +33,36 @@ Swiat::Swiat(int width, int height)
 //	delete[](map);
 //}
 
-	void Swiat::wykonajTure()
-	{
-		cout << "wykonana tura";
-	}
+void Swiat::wykonajTure()
+{
+	cout << "wykonana tura";
+}
 
-	void Swiat::rysujSwiat()
+void Swiat::rysujSwiat()
+{
+	for (int i = 0; i <= height * 2; i++)
 	{
-		for (int i = 0; i <= height * 2; i++)
+		for (int j = 0; j <= width * 2; j++)
 		{
-			for (int j = 0; j <= width * 2; j++)
-			{
-				if (i % 2 == 0 && j%2 == 0) cout << "+";
-				else if (i % 2 == 0 && j % 2 == 1) cout << "-";
+			if (i % 2 == 0 && j%2 == 0) cout << "+";
+			else if (i % 2 == 0 && j % 2 == 1) cout << "-";
 
-				if (i % 2 == 1 && j % 2 == 0)cout << "|";
-				if (i % 2 == 1 && j % 2 == 1) map[i/2][j/2]->rysowanie();
-			}
-			cout << endl;
+			if (i % 2 == 1 && j % 2 == 0)cout << "|";
+			if (i % 2 == 1 && j % 2 == 1) map[j/2][i/2]->rysowanie();
 		}
+		cout << endl;
 	}
+}
+
+Organizm* Swiat::losujOrganizm()
+{
+	//Organizm *organizmy[] = { new Czlowiek(1,2,this,2,1) };
+	int rand;
+	switch (rand)
+	{
+	case 1:
+		return new Czlowiek(1, 2, this, 2, 1);
+	default:
+		break;
+	}
+}
